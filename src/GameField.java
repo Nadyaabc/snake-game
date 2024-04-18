e54c7422a08948699c97fe905c3518cc09d93655
Nadejda <nadyash295923@gmail.com>
2024-04-18 22:31:53 +0300

    gamefield upd

--- src/GameField.java
+++ src/GameField.java
@@ -1,7 +1,32 @@
 import javax.swing.*;
 import java.awt.*;
 public class GameField extends JPanel{
+    private final int SIZE = 320;
+    private final int DOT_SIZE = 16;
+    private final int DOTS = 400;
+    private Image dot;
+    private Image apple;
+    private int appleX;
+    private int appleY;
+    private int [] x = new int[DOTS];
+    private int [] y = new int[DOTS];
+    private int dots;
+    private Timer timer;
+    private boolean right = true;
+    private boolean left;
+    private boolean up;
+    private boolean down;
+    private boolean inGame = true;
 
+    public GameField()
+    {
+        setBackground(Color.PINK);
+        loadImages();
+    }
 
+    public void loadImages()
+    {
+        ImageIcon
+    }
 
 }
